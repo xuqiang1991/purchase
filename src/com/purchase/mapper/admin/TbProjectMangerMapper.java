2b01c93bcf2c51c73a28b5a80e49cebdbb672289
zhoujb <zhoujianbinllyp@sina.com>
2018-08-20 09:51:55 +0800

    项目管理及启动报错修复

--- src/com/purchase/mapper/admin/TbProjectMangerMapper.java
+++ src/com/purchase/mapper/admin/TbProjectMangerMapper.java
@@ -2,8 +2,9 @@ package com.purchase.mapper.admin;
 
 import com.purchase.pojo.admin.TbProjectManger;
 import com.purchase.pojo.admin.TbProjectMangerExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TbProjectMangerMapper {
     int countByExample(TbProjectMangerExample example);
